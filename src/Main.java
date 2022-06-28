59aae0ebaf526eb37bb61ce8fdb6b2ce5580033d
vladicon <vladislav.balabanov@ulbsibiu.ro>
2022-06-28 09:57:07 +0200

    practica bit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import test.Tutor;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Tutor tutor = new Tutor();
+    }
+}
---
+++ src/Tutor.java
@@ -0,0 +1,10 @@
+//package test;
+
+public class Tutor {
+
+        List<Teacher> teacherList=new ArrayList<Teacher>();
+        public void run(){
+
+        }
+
+}
